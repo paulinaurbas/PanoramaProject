f59234bf10d5ea4f29c05a3a5d1de2f540ea2c69
paulinaurbas <urbas.paulina@gmail.com>
2018-08-07 13:57:23 +0200

    PanoramaProject

---
+++ PanoramaProje/Main.cpp
@@ -0,0 +1,56 @@
+// CPP program to Stitch
+// input images (panorama) using OpenCV 
+#include <iostream>
+#include <fstream>
+ 
+// Include header files from OpenCV directory
+// required to stitch images.
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/stitching.hpp"
+ 
+using namespace std;
+using namespace cv;
+ 
+// Define mode for stitching as panoroma 
+// (One out of many functions of Stitcher)
+Stitcher::Mode mode = Stitcher::PANORAMA;
+ 
+// Array for pictures
+vector<Mat> imgs;
+ 
+int main(int argc, char* argv[])
+{
+    // Get all the images that need to be 
+    // stitched as arguments from command line 
+	imgs.push_back(imread("1.jpg"));
+	imgs.push_back(imread("2.jpg"));
+	imgs.push_back(imread("3.jpg"));
+	imgs.push_back(imread("4.jpg"));
+    // Define object to store the stitched image
+    Mat pano;
+     
+    // Create a Stitcher class object with mode panoroma
+    Ptr<Stitcher> stitcher = Stitcher::create(mode, false);
+     
+    // Command to stitch all the images present in the image array
+    Stitcher::Status status = stitcher->stitch(imgs, pano);
+ 
+    if (status != Stitcher::OK)
+    {
+        // Check if images could not be stiched
+        // status is OK if images are stiched successfully
+        cout << "Can't stitch images\n";
+        return -1;
+    }
+     
+    // Store a new image stiched from the given 
+    //set of images as "result.jpg"
+    imwrite("result.jpg", pano);
+     
+    // Show the result
+    imshow("Result", pano);
+     
+    waitKey(0);
+    return 0;
+}
